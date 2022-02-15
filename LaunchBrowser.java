74eb8102b4cdfc69a9e87b2ceb1beeaf12f018fe
Elumalai66 <77559611+Elumalai66@users.noreply.github.com>
2022-02-15 11:34:36 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,22 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver=new ChromeDriver();
+	
+	driver.get("http//www.leaftaps.com/opentaps/control/main");
+	driver.manage().window().maximize();
+	
+
+	}
+
+	}
+
+
